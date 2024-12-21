b65df8f2a4adea8b79db46fd248109524a59b2f4
Prasun Suwal <prasun.suwal@gmail.com>
2024-12-21 08:54:25 +0545

    qn_no1

---
+++ LAB_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            // Prompt user to enter the second number
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            // Compute the result
+            int result = (num1 == num2) ? 3 * (num1 + num2) : num1 + num2;
+
+            // Display the result
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
