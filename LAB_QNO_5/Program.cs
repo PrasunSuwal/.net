27517dc1030cb9c00414aee40256f14410fa088c
Prasun Suwal <prasun.suwal@gmail.com>
2024-12-21 10:31:39 +0545

    qn_no5

---
+++ LAB_QNO_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_QNO_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter their age
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            // Print the message
+            Console.WriteLine($"You look older than {age}.");
+        }
+    }
+}
